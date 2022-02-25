c0f3baec8063e51860edcfcaf9611739a4de3d83
vietanhyoyo <99713924+vietanhyoyo@users.noreply.github.com>
2022-02-25 14:38:17 +0700

    update app.jss

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import './css/app.css';
-import Hearder from './components/Header';
-import Footer from './components/Footer';
 import Home from './components/home/Home';
 import Cart from './components/cart/Cart';
-import Chat from "./components/Chat";
 import Buy from "./components/buy/Buy";
 import Admin from "./components/admin/Admin";
 import Person from "./components/person/Person";
 import Client from "./components/Client";
 import PersonProfile from "./components/person/PersonProfile";
 import PersonHistory from "./components/person/PersonHistory";
+import PersonAddress from "./components/person/PersonAddress";
 
 function App() {
   return (
     <React.Fragment>
-      <Hearder />
-      <div style={{ paddingTop: '62px' }}></div>
       <Routes>
         <Route path='/' element={<Client />} >
           <Route path='home' element={<Home />} />
           <Route path='cart/:id' element={<Cart />} />
           <Route path='buy/:id' element={<Buy />} />
           <Route path='person' element={<Person />} >
-            <Route path='pro/:id' element={<PersonProfile />} />
-            <Route path='his/:id' element={<PersonHistory />} />
+            <Route path='profile/:id' element={<PersonProfile />} />
+            <Route path='history/:id' element={<PersonHistory />} />
+            <Route path='address/:id' element={<PersonAddress />} />
           </Route>
         </Route>
         <Route path='/admin/:id' element={<Admin />} ></Route>
       </Routes>
-      <Footer />
-      <Chat />
     </React.Fragment>
   );
 }
