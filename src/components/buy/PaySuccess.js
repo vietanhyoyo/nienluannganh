83ed0a577f54cad3cc2b6154b82cde2cfd6bb36b
votrungkien2000 <kiensua2000@gmail.com>
2022-04-17 17:17:03 +0700

    updatethongtinKH

--- src/components/buy/PaySuccess.js
+++ src/components/buy/PaySuccess.js
@@ -1,8 +1,24 @@
+import '../../css/paysuccess.css'
+import { Link } from 'react-router-dom'
 
 export default function PaySuccess() {
     return (
-        <div>
-            <h1>Thanh toán thành công!</h1>
+        <div class='pay row-app'>
+            <div class='pay_notify'>
+                <div class='pay_icon'>
+                    <i class=" pay_check_success fa-solid fa-circle-check"></i>
+                </div>
+                <div class='pay_notify_detail'>
+                    <h1>Thanh toán thành công!</h1>
+                    <h2>Cảm ơn bạn đã đến và mua hàng của chúng tôi!</h2>
+                </div>
+                <Link to='/'>
+                    <button class='button button_back'>Tiếp tục mua hàng</button>
+                </Link>
+                <Link to='/cart' >
+                    <p className='back_cart'>Kiểm tra giỏ hàng</p>
+                </Link>
+            </div>
         </div>
     )
 }
